604725372f18e541ff85899cda944dc71914d350
Mahdi Valizadeh <m.valizadeh@live.com>
2019-10-12 15:21:22 +0800

    CRUDService interface implemented as an abstract calss.

---
+++ src/libs/user-directory/typeorm/services/crud.service.ts
@@ -0,0 +1,31 @@
+import { ICRUDService } from '../../interfaces/services/interfaces/crud-service.interface';
+import { Repository , FindConditions, DeleteResult } from 'typeorm';
+
+export abstract class CRUDService<T> implements ICRUDService<T> {
+  constructor(protected repository: Repository<T>) {}
+  create(entity: T): T {
+    return this.repository.create(entity);
+  }
+
+  save(entity: T): Promise<T>{
+    return this.repository.save<T>(entity);
+  }
+
+  update(condition: number | Partial<T>, entity: T) {
+    return this.repository.update(condition as unknown as FindConditions<T>, entity);
+  }
+
+  delete(id: number | number[] | object | Partial<T>): Promise<DeleteResult> {
+    return this.repository.delete(id as FindConditions<T>);
+  }
+  find(condition?: object): Promise<T[]> {
+    return this.repository.find(condition);
+  }
+  findById(id: number): Promise<T> {
+    return this.repository.findOne(id);
+  }
+
+  findByIds(ids: number[]) {
+      return this.repository.findByIds(ids);
+  }
+}
