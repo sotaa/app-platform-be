7f566bc2916f36d2c2bd3f83301ffb9fa4417e37
Mahdi Valizadeh <m.valizadeh@live.com>
2019-10-12 15:20:32 +0800

    Interface enhanced.

--- src/libs/user-directory/interfaces/services/interfaces/crud-service.interface.ts
+++ src/libs/user-directory/interfaces/services/interfaces/crud-service.interface.ts
@@ -1,7 +1,9 @@
 export interface ICRUDService<T> {
     create(entity: T) : T | Promise<T>;
-    update(id: number, entity: T): T | Promise<T>;
-    delete(id: number): object;
-    get(condition?: object): T[] | Promise<T[]>;
-    getById(id: number): T | Promise<T>;
+    save(entity: T): T | Promise<T>;
+    update(condition: number | string | Partial<T>, entity: Partial<T>) : Promise<object>;
+    delete(id: number | string | number[] | string[] | Partial<T>): object | Promise<object>;
+    find(condition?: object): T[] | Promise<T[]>;
+    findById(id: number | string): T | Promise<T>;
+    findByIds(ids: number[] | string[]): T[] | Promise<T[]>;
 }
