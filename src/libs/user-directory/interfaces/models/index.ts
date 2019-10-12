b8e687b4be6caa7c41f26b952fcb86732f311e52
Mahdi Valizadeh <m.valizadeh@live.com>
2019-10-12 19:09:31 +0800

    Interfaces export configured.

---
+++ src/libs/user-directory/interfaces/models/index.ts
@@ -0,0 +1,5 @@
+export { IApplication } from './application.interface';
+export { IInvoice } from './invoice.interface';
+export { IPaymentPlan } from './payment-plan.interface';
+export { IToken } from './token.interface';
+export { IUser } from './user.interface';
---
+++ src/libs/user-directory/interfaces/services/index.ts
@@ -0,0 +1,2 @@
+export { ICRUDService } from './interfaces/crud-service.interface';
+export { IPaymentPlanService } from './interfaces/payment-plan-service.interface';
