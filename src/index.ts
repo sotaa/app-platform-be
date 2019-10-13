3c606c981c3f429f6a786ff34971af88cdd5dc23
Mahdi Valizadeh <m.valizadeh@live.com>
2019-10-14 02:04:56 +0800

    .env configuration added.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { UserDirectoryServer } from './user-directory.server';
+import { config as envConfig } from 'dotenv';
+
+envConfig();
+const env = process.env;
 
 const userDirectory = new UserDirectoryServer(console.log);
 userDirectory.enableDocumentation('/docs');
 userDirectory.initialize({dbConfig: {
-    type: 'mysql',
-    host: 'localhost',
-    database: 'userdirectory',
-    password: '@123qwe@',
-    port: '3306',
-    username: 'root',
-    synchronize: true
+    type: env.DB_TYPE as 'mysql' | 'mssql',
+    host: env.DB_HOST,
+    database: env.DB_NAME,
+    port: env.DB_PORT,
+    username: env.DB_USER,
+    password: env.DB_PASSWORD,
+    synchronize: eval(env.DB_SYNC)
 }});
 
-userDirectory.start(process.env.PORT || 3000);
+userDirectory.start(env.PORT || 3000);
