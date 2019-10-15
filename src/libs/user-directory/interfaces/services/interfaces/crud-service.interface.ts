91e25c1ee8d088efe8a5bf5017ba44967fe5c893
Mahdi Valizadeh <m.valizadeh@live.com>
2019-10-16 01:18:10 +0800

    Additional comment on update method added.

--- src/libs/user-directory/interfaces/services/interfaces/crud-service.interface.ts
+++ src/libs/user-directory/interfaces/services/interfaces/crud-service.interface.ts
@@ -1,6 +1,11 @@
 export interface ICRUDService<T> {
     create(entity: T) : T | Promise<T>;
     save(entity: T): T | Promise<T>;
+    /**
+     * update the entity with specific id or condition.
+     * @param condition object id or an object that represent the condition.
+     * @param entity the values needed to be updated.
+     */
     update(condition: number | string | Partial<T>, entity: Partial<T>) : Promise<object>;
     delete(id: number | string | number[] | string[] | Partial<T>): object | Promise<object>;
     find(condition?: object): T[] | Promise<T[]>;
