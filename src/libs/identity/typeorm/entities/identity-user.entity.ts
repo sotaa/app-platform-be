e661fbd3b22b5cbd85ae52f8199a928023bce78b
Mahdi Valizadeh <m.valizadeh@live.com>
2019-10-29 23:43:57 +0800

    Username unique idenifyer added.

--- src/libs/identity/typeorm/entities/identity-user.entity.ts
+++ src/libs/identity/typeorm/entities/identity-user.entity.ts
@@ -1,5 +1,5 @@
 import { IIdentityUser, IToken } from '../../interfaces';
-import { Entity, Column, OneToMany, TableInheritance, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, OneToMany, TableInheritance, PrimaryGeneratedColumn, Index } from 'typeorm';
 import { Token } from './token.entity';
 import { IEntity } from '../interfaces';
 
@@ -9,6 +9,7 @@ export abstract class IdentityUser implements IEntity, IIdentityUser {
   @PrimaryGeneratedColumn()
   id?: string;
   @Column()
+  @Index({unique: true})
   username: string;
   @Column()
   password: string;
