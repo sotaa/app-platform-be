488c9d5fcfed8a3fe745ed4b9f503daa4e2253c5
Mahdi Valizadeh <m.valizadeh@live.com>
2019-11-02 13:10:48 +0800

    Remove redundant invoice service.

--- src/libs/user-directory/typeorm/services/index.ts
+++ src/libs/user-directory/typeorm/services/index.ts
@@ -1,4 +1,3 @@
 export { PaymentPlanService } from './payment-plan.service';
 export { ApplicationService } from './application.service';
-export { InvoiceService } from './invoice.service';
 export { UserService } from './user.service';
