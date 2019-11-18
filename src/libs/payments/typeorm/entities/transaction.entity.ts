9969a316d7b43379a7467aa48105d048455376a7
Meitix <m.valizadeh@live.com>
2019-11-18 14:25:47 +0800

    Transaction db schema added.

---
+++ src/libs/payments/typeorm/entities/transaction.entity.ts
@@ -0,0 +1,27 @@
+import { EntitySchema } from "typeorm";
+import { Transaction, TransactionStatus } from "../../lib";
+
+export const TransactionEntity = new EntitySchema<Transaction>({
+    name: 'transaction',
+    columns: {
+        transactionKey: {
+            type: String,
+            nullable: false,
+            unique: true
+        },
+        status: {
+            type: Number,
+            enum: TransactionStatus,
+            nullable: false
+        }
+    },
+    relations: {
+        invoice: {
+            type: 'many-to-one',
+            target: 'invoice',
+            nullable: false,
+            lazy: true
+        }
+    }
+    
+})
