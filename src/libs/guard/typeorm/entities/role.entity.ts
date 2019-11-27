354a09921a69e037769147e52df41184941eacb8
Meitix <m.valizadeh@live.com>
2019-11-27 17:37:17 +0800

    Role entity enhanced.

--- src/libs/guard/typeorm/entities/role.entity.ts
+++ src/libs/guard/typeorm/entities/role.entity.ts
@@ -1,25 +1,37 @@
 import { EntitySchema } from 'typeorm';
 import { IRole } from '../../';
 
-export const RoleEntity = new EntitySchema<IRole>({
-    name: 'role',
+/**
+ * Returns a configured typeorm EntitySchema instance for roles.
+ * @param entityName The name of entity
+ * @param userEntityName User entity name
+ * @param userRolesPropertyName The property name which defines roles array in property entity.
+ * @param tableName Name of the table for storing roles data in it.
+ */
+export const RoleEntity = (
+  entityName: string,
+  userEntityName: string,
+  userRolesPropertyName: string,
+  tableName: string = entityName,
+) =>
+  new EntitySchema<IRole>({
+    name: entityName,
+    tableName: tableName,
     columns: {
-        title: {
-            type: String,
-            primary: true,
-            nullable: false
-        },
-        permissions: {
-            type: 'simple-array',
-            array: true
-        }
+      title: {
+        type: String,
+        primary: true,
+        nullable: false
+      },
+      permissions: {
+        type: 'simple-array',
+      }
     },
     relations: {
-        users: {
-            type: 'many-to-many',
-            cascade: true,
-            target: 'guard_user',
-            inverseSide: 'roles'
-        }
+      users: {
+        type: 'many-to-many',
+        target: userEntityName,
+        inverseSide: userRolesPropertyName
+      }
     }
-});
+  });
