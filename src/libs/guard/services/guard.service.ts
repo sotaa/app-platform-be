fcc65a7517fd3401d1a9d65fe862b999b4829a12
soheil najafi <soheilnajafimail2@gmail.com>
2020-03-14 10:52:03 +0330

    handle if parent doesnt exist and role is already exist

--- src/libs/guard/services/guard.service.ts
+++ src/libs/guard/services/guard.service.ts
@@ -1,10 +1,12 @@
+import { BAD_REQUEST } from 'http-status-codes';
+import { ErrorCodes } from './../../payments/lib/enums/error-codes.eum';
 import { IGuardUser, IRole, IGuardService } from '../interfaces';
-import { intersection } from 'lodash';
+import { intersection, difference } from 'lodash';
 import { IRoleRepository } from '../interfaces/role.repository';
-import { RoleHasUserError } from '../classes/errors';
+import { RoleHasUserError, RoleAlreadyExists, PermissionsNotAllowed } from '../classes/errors';
 
 export class GuardService implements IGuardService {
-  constructor(protected roleRepo: IRoleRepository) {}
+  constructor(protected roleRepo: IRoleRepository) { }
   /**
    * check whether the use has all of required permissions or not.
    * @param user user
@@ -25,11 +27,40 @@ export class GuardService implements IGuardService {
 
     if (requiredPermissions.length > userPermissions.length) return false;
 
-    return intersection(userPermissions, requiredPermissions).length === requiredPermissions.length;
+    // return intersection(userPermissions, requiredPermissions).length === requiredPermissions.length;
+    return difference(requiredPermissions, userPermissions).length === 0;
   }
 
-  createRole(role: IRole) {
-    return this.roleRepo.save(role);
+  permissionsAreAllowed(role: IRole): boolean {
+    if (role.permissions.length > role.parent.permissions.length) { return false };
+    if (difference(role.permissions, role.parent.permissions).length > 0) { return false } else return true;
+  }
+
+  async createRole(role: IRole, user: IGuardUser): Promise<any> {
+    let title = role.title;
+    const roleExist = await this.roleRepo.findOne({ where: { title } });
+
+    if (roleExist) {
+      throw new RoleAlreadyExists();
+    }
+
+    if (!role.parent) {
+      role.parent = user.role;
+    }
+
+    if (this.permissionsAreAllowed(role)) {
+      return this.roleRepo.save(role);
+    } else throw new PermissionsNotAllowed();
+  }
+
+  updateRole(title: string, role: IRole, user: IGuardUser): any {
+    if (!role.parent) {
+      role.parent = user.role;
+    }
+    if (this.permissionsAreAllowed(role)) {
+      return this.roleRepo.update({ title }, role);
+    } else throw new PermissionsNotAllowed();
+
   }
 
   addOrUpdate(role: IRole): Promise<IRole> {
@@ -42,11 +73,7 @@ export class GuardService implements IGuardService {
   }
 
   findRoleByTitle(title: string) {
-    return this.roleRepo.findOne({ where: { title } });
-  }
-
-  updateRole(title: string, role: IRole) {
-    return this.roleRepo.update({ title }, role);
+    return this.roleRepo.findOne({ where: { title }, relations: ['children'] });
   }
 
   async deleteRole(title: string) {
