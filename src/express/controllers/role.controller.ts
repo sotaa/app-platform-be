13f6e9326a9c76f7dfac398dfb25a3b187d5c52b
soheil najafi <soheilnajafimail2@gmail.com>
2020-03-14 10:39:09 +0330

    pass user to create and update method

--- src/express/controllers/role.controller.ts
+++ src/express/controllers/role.controller.ts
@@ -5,21 +5,25 @@ import { TYPES } from '../../ioc/types';
 
 @injectable()
 export class RoleController {
-  constructor(@inject(TYPES.IGuardService) protected guardService: IGuardService) {}
+  constructor(@inject(TYPES.IGuardService) protected guardService: IGuardService) { }
 
   fetch(req: IRequest): Promise<IRole[]> {
     const user = (req as any).user as IGuardUser;
     return this.guardService.findRolesByParentTitle(user.role.title);
   }
+  fetchByTitle(title: string): Promise<IRole> {
+    return this.guardService.findRoleByTitle(title);
+  }
 
-  update(title: string, role: IRole): Promise<IRole> {
-    return this.guardService.updateRole(title, role);
+  update(title: string, role: IRole, user: IGuardUser): Promise<IRole> {
+    return this.guardService.updateRole(title, role, user);
   }
 
   // TODO: This method should check user permissions, cause a user can create a role with permissions same or less than itself.>>>
   // TODO: >>> in the other word creator permissions must include new role permissions and the permissions are not in creator permissions are not allowed.
-  create(role: IRole): Promise<IRole> {
-    return this.guardService.createRole(role);
+
+  create(role: IRole, user: IGuardUser): Promise<IRole> {
+    return this.guardService.createRole(role, user);
   }
 
   delete(title: string): Promise<any> {
