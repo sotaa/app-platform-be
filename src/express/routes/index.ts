6adca51d7d65061e78cca946ab2b7852457cc2aa
soheil najafi <soheilnajafimail2@gmail.com>
2020-03-14 10:42:20 +0330

    fix payment Route problem

--- src/express/routes/index.ts
+++ src/express/routes/index.ts
@@ -2,11 +2,19 @@ import { ILogger } from '../../logger';
 import { Router } from 'express';
 import { iocContainer, TYPES } from '../../ioc';
 import { applicationRouter } from './application.routes';
-import { ApplicationController, AuthController, RoleController, UserController, PaymentPlansController } from '../controllers';
+import {
+  ApplicationController,
+  AuthController,
+  RoleController,
+  UserController,
+  PaymentPlansController,
+  PaymentController
+} from '../controllers';
 import { authRouter } from './auth.routes';
 import { roleRouter } from './role.routes';
 import { userRouter } from './user.routes';
 import { paymentPlanRouter } from './payment-plan.routes';
+import { paymentRouter } from './payment.routes';
 
 class UserDirectoryRouter {
   router: Router;
@@ -17,9 +25,9 @@ class UserDirectoryRouter {
   init() {
     this.router.use('/applications', applicationRouter(iocContainer.get(ApplicationController), this.logger));
     this.router.use('/auth', authRouter(iocContainer.get(AuthController), this.logger));
-    this.router.use('/payment', authRouter(iocContainer.get(AuthController), this.logger));
+    this.router.use('/payment', paymentRouter(iocContainer.get(PaymentController), this.logger));
     this.router.use('/payment-plans', paymentPlanRouter(iocContainer.get(PaymentPlansController), this.logger));
-    this.router.use('/role', roleRouter(iocContainer.get(RoleController), this.logger));
+    this.router.use('/roles', roleRouter(iocContainer.get(RoleController), this.logger));
     this.router.use('/user', userRouter(iocContainer.get(UserController), this.logger));
   }
 }
