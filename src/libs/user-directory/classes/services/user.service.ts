e6888541940f764f5e9e792d8737efdb042095b7
soheil najafi <soheilnajafimail2@gmail.com>
2020-03-28 13:36:25 +0430

    create user mthod service

--- src/libs/user-directory/classes/services/user.service.ts
+++ src/libs/user-directory/classes/services/user.service.ts
@@ -1,5 +1,5 @@
 import { IUser } from '../../interfaces/';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { User } from '../models';
 import { IUserService } from '../../interfaces/services/user.service.interface';
 
@@ -10,10 +10,19 @@ export class UserService implements IUserService {
     return this.repository.save(user);
   }
 
-  findById(id: string): Promise<IUser> {
-    return this.repository.findOne(id, { relations: ['role']});
+  update(id: string, user: User): Promise<any> {
+    return this.repository.update(id, user);
   }
+
+  findAll(condition?: object): Promise<IUser[]> {
+    return this.repository.find({ relations: ['role'] });
+  }
+
+  findById(id: string): Promise<IUser> {
+    return this.repository.findOne(id, { relations: ['role'] });
+  }
+
   getByRoleTitle(roleTitle: string): Promise<IUser[]> {
-    return this.repository.find({ where: { role: { title: roleTitle } },relations: ['role'] });
+    return this.repository.find({ where: { role: { title: roleTitle } }, relations: ['role'] });
   }
 }
--- src/libs/user-directory/interfaces/services/user.service.interface.ts
+++ src/libs/user-directory/interfaces/services/user.service.interface.ts
@@ -1,7 +1,9 @@
 import { IUser } from '../models/user.interface';
 
 export interface IUserService {
-    create(user: IUser): Promise<IUser>;
-    findById(id: string): Promise<IUser>;
-    getByRoleTitle(roleTitle: string): Promise<IUser[]>;
+  create(user: IUser): Promise<IUser>;
+  update(email: string, user: IUser): Promise<any>;
+  findById(id: string): Promise<IUser>;
+  findAll(filter?: any): Promise<IUser[]>;
+  getByRoleTitle(roleTitle: string): Promise<IUser[]>;
 }
