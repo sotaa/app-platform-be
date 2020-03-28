456ba5d7382ae84e54a154d7d1a64802ceaaeb60
soheil najafi <soheilnajafimail2@gmail.com>
2020-03-28 13:45:47 +0430

    add methods in user controller

--- src/express/controllers/user.controller.ts
+++ src/express/controllers/user.controller.ts
@@ -1,36 +1,45 @@
-import { Request as IRequest} from 'express';
+import { Request as IRequest } from 'express';
 import { IUser, IUserService } from '../../libs/user-directory/interfaces';
 import { inject, injectable } from 'inversify';
 import { TYPES } from '../../ioc/types';
-import { IIdentityUser } from '../../libs/identity/interfaces';
+import { IGuardService } from '../../libs/guard/interfaces/guard.service.interface';
+import { USER_EDIT } from '../../config/permissions.const';
+
+import { DefaultRole } from '../../config/default-role';
 
 @injectable()
 export class UserController {
-  constructor(@inject(TYPES.IUserService) private userService: IUserService) {
-  }
-  
-  // @Get()
-  // async list(@Query() filter?: any): Promise<IUser[]> {
-  //   return this.userService.find(filter);
-  // }
+  constructor(
+    @inject(TYPES.IUserService) private userService: IUserService,
+    @inject(TYPES.IGuardService) private gaurdService: IGuardService
+  ) {}
 
-  // @Get('{id}')
-  // async find(id: number): Promise<IUser> {
-  //   return this.userService.findById(id);
-  // }
-  
-  create(user: IUser, req: IRequest): Promise<IUser> {
-    const idUser = (req.body as any).user as IIdentityUser;
-    user.id = idUser.id.toString();
-    return this.userService.create(user);
+  getAll(filter?: any): Promise<IUser[]> {
+    return this.userService.findAll(filter);
   }
 
+  get(id: string): Promise<IUser> {
+    return this.userService.findById(id);
+  }
 
-  // @Put('{id}')
-  // async update(id:number, @Body() user: IUser): Promise<any> {
-  //   return this.userService.update(id, user);
+  async update(id: string, reqBody: any, user: any): Promise<any> {
+    let role;
+    user.role.permissions.find((f: any) => f == USER_EDIT)
+      ? (reqBody.role ? (role = reqBody.role) : (role = DefaultRole),
+        (reqBody.role = await this.gaurdService.findRoleByTitle(role)))
+      : delete reqBody.role;
+    reqBody.gender ? (reqBody.gender = reqBody.gender.toUpperCase()) : true;
+    reqBody.email ? delete reqBody.email : true;
+
+    return this.userService.update(id, reqBody);
+  }
+
+  // create(user: IUser, req: IRequest): Promise<IUser> {
+  //   const idUser = (req.body as any).user as IIdentityUser;
+  //   user.id = idUser.id.toString();
+  //   return this.userService.create(user);
   // }
-  
+
   // @Delete('{id}')
   //  async delete(id: number): Promise<any> {
   //    return this.userService.delete(id);
