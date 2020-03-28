e6f0e18741f71cc6318837fdf62a1a3b5a9d5fe4
soheil najafi <soheilnajafimail2@gmail.com>
2020-03-28 13:11:46 +0430

    add user default role in seeder file

--- src/services/bootstrap/seeder.ts
+++ src/services/bootstrap/seeder.ts
@@ -1,5 +1,8 @@
+import { IRoleRepository } from './../../libs/guard/interfaces/role.repository';
 import { IRole, Role, IGuardService } from '../../libs/guard';
 import * as permissionsObject from '../../config/permissions.const';
+import * as defaultUserPermissionsObject from '../../config/user-default-permissions.const';
+import { DefaultRole } from '../../config/default-role';
 import { IAuthService, IAuthData } from '../../libs/identity';
 import { User } from '../../libs/user-directory/classes';
 import { values } from 'lodash';
@@ -7,17 +10,26 @@ import { iocContainer, TYPES } from '../../ioc';
 import { IUserService } from '../../libs/user-directory';
 
 const permissions = values(permissionsObject);
+const defaultUserPermissions = values(defaultUserPermissionsObject);
 
 const SUPER_ADMIN_ROLE_TITLE = 'Super Admin';
 
 export async function seedDB(seedValues: any) {
-  const superAdmin = await createOrUpdateSuperAdminRole();
-  createSuperAdminUsers(seedValues.superAdmins, superAdmin);
+  // const superAdmin = await createOrUpdateSuperAdminRole();
+  await createOrUpdateDefaultUserRole();
+  // createSuperAdminUsers(seedValues.superAdmins, superAdmin);
 }
 
 function createOrUpdateSuperAdminRole() {
   const superAdmin = new Role(SUPER_ADMIN_ROLE_TITLE, permissions);
   return iocContainer.get<IGuardService>(TYPES.IGuardService).addOrUpdate(superAdmin);
+}
+
+async function createOrUpdateDefaultUserRole() {
+  const roleService = await iocContainer.get<IGuardService>(TYPES.IGuardService);
+  const superAdminRole = await roleService.findRoleByTitle(SUPER_ADMIN_ROLE_TITLE);
+  const role = new Role(DefaultRole, defaultUserPermissions, superAdminRole);
+  return roleService.addOrUpdate(role);
 }
 
 async function createSuperAdminUsers(users: IAuthData[], superAdminRole: IRole) {
